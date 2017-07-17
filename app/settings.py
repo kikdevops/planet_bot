8874c0872006ea5b673430dca5d10d18e1ff6b0f
Nikolay Osmolovsky <odmin666@gmail.com>
2017-07-17 12:03:46 +0300

    Message

--- app/settings.py
+++
@@ -1,9 +0,0 @@
-token = '771f009d4382384ff7ce6dfd22ccbd227812b05cf25397fd2d6fa88cc9de598cadcdfe1aecf0b794d0f16'
-confirmation_token = 'ce4cf456'
-
-#id of standalone app
-hello_app_id = 6113618
-
-#Get user token with
-#https://oauth.vk.com/authorize?client_id=6113618&scope=notify,photos,friends,audio,video,notes,pages,docs,status,questions,offers,wall,groups,messages,notifications,stats,ads,offline&redirect_uri=http://api.vk.com/blank.html&display=page&response_type=token
-user_token = 'a04e7de10764122fa8cc09bcaf1be7f49b1ef97d68addc007a1e81c3e8b87939263fd1222c3d911ade81e'
