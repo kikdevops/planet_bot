69790070405a9034d41cb16b7f61789130d5162a
Nikolay Osmolovsky <odmin666@gmail.com>
2017-07-20 13:20:13 +0300

    added logger

--- app/flask_app.py
+++ app/flask_app.py
@@ -29,4 +29,13 @@ def processing():
         messageHandler.create_hello(data['object'], user_token)
         return 'ok'
 
-app.run(debug = True)
+if __name__ == '__main__':
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('planetbot.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('planet bot startup')
+    app.run(debug = False)
